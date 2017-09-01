0d1065c46befa6fa4d488bf48e3829135e4e2d41
Taziev Rodion <morrowcj81@gmail.com>
2017-09-02 02:33:03 +0700

    movavi 85%

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
         $(".full-mode").show();
         $(this).addClass("active");
         $(".easy").removeClass("active");
+        $(".triangle-full").show();
+        $(".triangle-easy").hide();
     });
 
     $(".easy").click(function () {
@@ -12,12 +14,18 @@ $(document).ready(function () {
         $(".full-mode").hide();
         $(this).addClass("active");
         $(".fullf").removeClass("active");
+        $(".triangle-full").hide();
+        $(".triangle-easy").show();
     });
 
 
     $(".switch-full").bind("click", function (event) {
         $(".easy-mode").hide();
         $(".full-mode").show();
+        $(".fullf").addClass("active");
+        $(".easy").removeClass("active");
+        $(".triangle-full").show();
+        $(".triangle-easy").hide();
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
@@ -27,6 +35,10 @@ $(document).ready(function () {
     $(".switch-easy").bind("click", function (event) {
         $(".easy-mode").show();
         $(".full-mode").hide();
+        $(".easy").addClass("active");
+        $(".fullf").removeClass("active");
+        $(".triangle-full").hide();
+        $(".triangle-easy").show();
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
