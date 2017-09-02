d901940a365ee50dab8fd5a87cbbf9a849e4401d
Taziev Rodion <morrowcj81@gmail.com>
2017-09-02 23:53:42 +0700

    movavi 100%

--- js/main.js
+++ js/main.js
@@ -1,49 +1,91 @@
 $(document).ready(function () {
 
-    $(".fullf").click(function () {
-        $(".easy-mode").hide();
-        $(".full-mode").show();
-        $(this).addClass("active");
-        $(".easy").removeClass("active");
-        $(".triangle-full").show();
-        $(".triangle-easy").hide();
-    });
+    if (window.matchMedia("(min-width: 768px)").matches) {
 
-    $(".easy").click(function () {
-        $(".easy-mode").show();
-        $(".full-mode").hide();
-        $(this).addClass("active");
-        $(".fullf").removeClass("active");
-        $(".triangle-full").hide();
-        $(".triangle-easy").show();
-    });
+        $(".fullf").click(function () {
+            $(".easy-mode").hide();
+            $(".full-mode").show();
+            $(this).addClass("active");
+            $(".easy").removeClass("active");
+            $(".triangle-full").show();
+            $(".triangle-easy").hide();
+        });
+
+        $(".easy").click(function () {
+            $(".easy-mode").show();
+            $(".full-mode").hide();
+            $(this).addClass("active");
+            $(".fullf").removeClass("active");
+            $(".triangle-full").hide();
+            $(".triangle-easy").show();
+        });
 
 
-    $(".switch-full").bind("click", function (event) {
-        $(".easy-mode").hide();
-        $(".full-mode").show();
-        $(".fullf").addClass("active");
-        $(".easy").removeClass("active");
-        $(".triangle-full").show();
-        $(".triangle-easy").hide();
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1100);
-    });
+        $(".switch-full").bind("click", function (event) {
+            $(".easy-mode").hide();
+            $(".full-mode").show();
+            $(".fullf").addClass("active");
+            $(".easy").removeClass("active");
+            $(".triangle-full").show();
+            $(".triangle-easy").hide();
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1100);
+        });
 
-    $(".switch-easy").bind("click", function (event) {
-        $(".easy-mode").show();
-        $(".full-mode").hide();
-        $(".easy").addClass("active");
-        $(".fullf").removeClass("active");
-        $(".triangle-full").hide();
-        $(".triangle-easy").show();
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1100);
-    });
+        $(".switch-easy").bind("click", function (event) {
+            $(".easy-mode").show();
+            $(".full-mode").hide();
+            $(".easy").addClass("active");
+            $(".fullf").removeClass("active");
+            $(".triangle-full").hide();
+            $(".triangle-easy").show();
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1100);
+        });
 
+    } else {
+
+        $(".fullf").click(function () {
+            $(".easy-mode").hide();
+            $(".full-mode").show();
+            $(this).addClass("active");
+            $(".easy").removeClass("active");
+        });
+
+        $(".easy").click(function () {
+            $(".easy-mode").show();
+            $(".full-mode").hide();
+            $(this).addClass("active");
+            $(".fullf").removeClass("active");
+        });
+
+
+        $(".switch-full").bind("click", function (event) {
+            $(".easy-mode").hide();
+            $(".full-mode").show();
+            $(".fullf").addClass("active");
+            $(".easy").removeClass("active");
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1100);
+        });
+
+        $(".switch-easy").bind("click", function (event) {
+            $(".easy-mode").show();
+            $(".full-mode").hide();
+            $(".easy").addClass("active");
+            $(".fullf").removeClass("active");
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1100);
+        });
+
+    }
 
 });
